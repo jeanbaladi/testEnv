89571969c5a9e8c99b76317bd78ae5ca949c496b
Jean <yanbaladi159@gmail.com>
2024-10-14 10:48:24 -0300

    -Se crean dos rutas de compilacion distintas

--- scripts/set-enviroment.js
+++ scripts/set-enviroment.js
@@ -1,19 +1,33 @@
 const fs = require('fs');
-require('dotenv').config();
+require('dotenv').config({ path: `./scripts/.env` });
 
-const environmentProdFile = `export const environment = {
-  production: true,
-  API_URL: '${process.env.API_URL || 'api_url not found'}',
-  envs: ${JSON.stringify(process.env)}
-};
-`;
+const argument = process.argv[2];
+const api_url = () => {
+  if(argument == "COBO")
+    return process.env.API_URL_CO || 'api_url_co not found'
+  return process.env.API_URL || 'api_url not found'
+}
+const envs = JSON.stringify(process.env);
+const filePath = './src/environments/environment.prod.ts';
+console.log(`El argumento que pasaste es: ${argument}`);
 
-console.log(process.env.API_URL);
-
-fs.writeFile('../src/environments/environment.ts', environmentProdFile, function (err) {
+// Leer el archivo actual
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log('Environment file generated successfully.');
+    console.error('Error leyendo el archivo:', err);
+    return;
   }
+
+  // Usamos una expresión regular para encontrar y reemplazar el valor de API_URL
+  const updatedData = data.replace(/API_URL:\s*'[^']*'/, `API_URL: '${api_url()}'`);
+
+  // Sobreescribir el archivo con el contenido modificado
+  fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+    if (err) {
+      console.error('Error escribiendo el archivo:', err);
+      return;
+    }
+    console.log('Archivo environment.ts actualizado exitosamente.');
+  });
 });
+
